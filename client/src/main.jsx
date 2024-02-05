3b40aa62c4bc4ef92ca021139901b69f299bc48c
Gabriel-Viernes <viernesgabriel04@gmail.com>
2024-02-05 14:43:11 -0600

    created homepage, homepage now used by router as an index

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,13 +2,20 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { createBrowserRouter, RouterProvider } from "react-router-dom"
 
+import Homepage from './pages/Homepage.jsx'
 import App from './App.jsx'
 import './index.css'
 
 const router = createBrowserRouter([
     {
         path:"/",
-        element: <App />
+        element: <App />,
+        children: [
+            {
+                index: true,
+                element: <Homepage />
+            }
+        ]
     }
 ])
 
---
+++ client/src/pages/Homepage.jsx
@@ -0,0 +1,11 @@
+export default function Homepage() {
+    return (
+        <>
+            <div className="home">
+                <h1>Gurubooru</h1>
+                <input placeholder='Search tags here!'></input>
+            </div>
+            <button>Search</button>
+        </>
+    )
+}
