7ba56f3ecaa6c40e02855ca8d476d585f09395cb
Gabriel-Viernes <viernesgabriel04@gmail.com>
2024-02-10 18:11:48 -0600

    filename and username added to upload form as hidden p tags

--- client/src/pages/Upload.jsx
+++ client/src/pages/Upload.jsx
@@ -3,10 +3,8 @@ import { v4 as uuid } from 'uuid'
 import decode from 'jwt-decode'
 import Auth from '../../utils/auth.js'
 
-
-
-
 export default function Upload() {
+    let filename = uuid()
     let decoded;
     if(Auth.loggedIn() === false ) {
         window.location.assign('/login')
@@ -36,6 +34,8 @@ export default function Upload() {
             <form className='uploadForm' action='http://localhost:3002' onSubmit={handleFileUpload} method='post' encType='multipart/form-data'>
                 <input type='file' id='upload' name='upload'></input>
                 <textarea name='tags' value="Enter tags here, with each tag separated by a space. Tags with two words should have an underline between each word (Ex: sunset tail steam_engine)"></textarea>
+                <p style={{display: "none"}} value={decoded.data.username}></p>
+                <p stype={{display: "none"}} value={filename}></p>
                 <button className='submit' value="submit">Upload!</button>
             </form>
     )
