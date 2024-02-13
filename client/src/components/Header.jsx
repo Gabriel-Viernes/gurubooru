aaed24283140001db92478a8389f3023a24d8016
Gabriel-Viernes <viernesgabriel04@gmail.com>
2024-02-13 08:05:39 -0600

    added a searchbar to results.jsx

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,7 +1,26 @@
 import Auth from '../../utils/auth'
 import { Link, createSearchParams } from "react-router-dom"
+import { useState } from 'react'
 
 export default function Header() {
+    let regex = /(?<!\S)([a-z0-9]+)_([a-z0-9]+)(?!\S)|(?<!\S)([a-z0-9*]+)(?!\S)/gm
+    const [query, setQuery] = useState('')
+    let search = []
+    const [submit, setSubmit] = useState([])
+
+    const handleInput = function(event) {
+        const { name, value } = event.target
+        if(name === 'queryInput') {
+            setSubmit([])
+            search = []
+            setQuery(value)
+            let temp = [...value.matchAll(regex)]
+            for (const match of temp) {
+                search.push(match[0])
+            }
+            setSubmit(search)
+        }
+    }
     return (
         <div className="component-header">
             <Link to="/"><h1>Gurubooru</h1></Link>
@@ -16,9 +35,18 @@ export default function Header() {
                         <>
                             <li><Link to='/login'>Login</Link></li>
                             <li><Link to='/signup'>Signup</Link></li>
+                            <li><input onChange={handleInput} name='queryInput' placeholder='Search tags here!' value= {query}></input></li>
+                            <li><button><Link reloadDocument name="submit" to={{
+                    pathname:"",
+                    search:`?${createSearchParams({
+                        tags: submit
+                    })}`
+                }}>Search</Link></button></li>
+
                         </>
                     )
                 }
+
             </ul>
         </div>
 
--- client/src/pages/Loading.jsx
+++ client/src/pages/Loading.jsx
@@ -1,9 +1,5 @@
 export default function Loading() {
-    let temp = window.location.replace("/loading", "")
-    console.log(temp)
-
-
     return (
-        <h1>If you're seeing this page, the website is either loading or there is an error 404</h1>
+        <h1>Error 404: Page not found</h1>
     )
 }
